fdee3f120a5736fa7cd976837d52ba2e5a75f38f
grishavanika <grish.vanika@gmail.com>
2025-04-17 19:53:19 +0300

    simplest sender

--- main.cc
+++ main.cc
@@ -5,9 +5,11 @@
 #pragma warning(disable: 4459)
 #include <beman/execution/execution.hpp>
 namespace ex = beman::execution;
+static const int once = ([]{ std::println("-- beman::execution"); }(), 1);
 #elif __has_include(<stdexec/execution.hpp>)
 #include <stdexec/execution.hpp>
 namespace ex = stdexec;
+static const int once = ([]{ std::println("-- stdexec"); }(), 1);
 #endif
 
 #if defined(NDEBUG)
@@ -15,10 +17,44 @@ namespace ex = stdexec;
 #endif
 #include <cassert>
 
+template<typename Receiver>
+struct My_OperationState
+{
+    using operation_state_concept = ex::operation_state_t;
+    
+    void start() noexcept
+    {
+        ex::set_value(std::move(_receiver));
+    }
+
+    Receiver _receiver;
+};
+
+struct My_Sender
+{
+    using sender_concept = ex::sender_t;
+    using completion_signatures = ex::completion_signatures<ex::set_value_t()>;
+
+    template<typename Receiver
+        , typename Receiver_ = std::remove_cvref_t<Receiver>>
+    My_OperationState<Receiver_> connect(Receiver&& receiver)
+    {
+        return {std::forward<Receiver>(receiver)};
+    }
+};
+
 int main()
 {
-    std::optional<std::tuple<int>> x = ex::sync_wait(ex::just(1));
-    assert(x.has_value());
-    auto [v] = *x;
-    std::println("ex::just(1) - got {}", v);
+    {
+        std::optional<std::tuple<int>> x = ex::sync_wait(ex::just(1));
+        assert(x.has_value());
+        auto [v] = *x;
+        std::println("ex::just(1) - got {}", v);
+    }
+    {
+        std::optional<std::tuple<>> x = ex::sync_wait(My_Sender{});
+        assert(x.has_value());
+        std::println("My_Sender() - done");
+    }
 }
+
