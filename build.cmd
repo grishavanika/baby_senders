2b46a39def661d07d77b969e78aed144f25e0479
grishavanika <grish.vanika@gmail.com>
2025-06-22 23:19:23 +0300

    fix cmake build (GIT_SHALLOW=False)

--- build.cmd
+++ build.cmd
@@ -1,3 +1,7 @@
 call cmake -S . -B build -Wno-dev
 call cmake --build build --target beman_senders --config Debug
 call cmake --build build --target stdexec_senders --config Debug
+
+:: call cmake -S . -B build_clang -T ClangCL -Wno-dev
+:: call cmake --build build_clang --target beman_senders --config Debug
+:: call cmake --build build_clang --target stdexec_senders --config Debug
